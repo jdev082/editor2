139c18c63a16f91033afb74b48dfaf1a81ba99da
jdev082 <jdev082@jdev.eu.org>
2024-04-11 19:30:59 -0500

    feat: initial implementation of settings

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import { Editor } from "./editor/editor.js";
+const toolbarBtn = document.getElementById("toolbarBtn")
+const options = document.getElementById("options")
 
 let contents = btoa(`
 This div is editable
@@ -10,3 +12,7 @@ document.addEventListener("DOMContentLoaded", () => {
     let editor = new Editor(document.querySelector("#editor"), {useTab: true});
     editor.load(contents);
 });
+
+toolbarBtn.addEventListener("click", () => {
+    options.classList.toggle("hidden")
+})
